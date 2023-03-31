fd212cd30f74157938258a1105d3e8143f6602ca
Gotovskiy <Skeezmo_new@mail.ru>
2023-03-31 21:55:18 +0300

    added first 3d sneaker model

--- src/components/WebDising.jsx
+++ src/components/WebDising.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
 import Thinking from "./Thinking";
-import { OrbitControls} from "@react-three/drei";
+import Sneaker from "./Sneaker"
+import { OrbitControls, Stage, Tube} from "@react-three/drei";
+import { PerspectiveCamera, RenderTexture } from "@react-three/drei";
 
 function WebDisign() {
 
@@ -9,10 +11,12 @@ function WebDisign() {
 
     return ( 
     <Canvas >
-    <OrbitControls enableZoom={false} enableRotate={false} enablePan={false}/>
-    <ambientLight intensity={1}/>
+    <OrbitControls autoRotate={true} autoRotateSpeed={0.4} enableZoom={false}/>
+    <ambientLight intensity={2} color={"red"}/>
     <directionalLight position={[2,1,3]}/>
-    <Thinking/>
+    <Stage environment={"city"} intensity={0.4} >
+        <Sneaker/>
+        </Stage>
     </Canvas > );
 }
 
