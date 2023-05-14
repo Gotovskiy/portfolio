51e1c46f424a9f54f0b84af00828dc0bfbde7166
Gotovskiy <Skeezmo_new@mail.ru>
2023-05-14 21:15:20 +0300

    Added service

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -66,7 +66,7 @@ function Navbar({handleClickScrollToWho, handleClickScrollToWorks, handleClickSc
     <Section>
         <Containter>
         <Links>
-        <Logo src="./img/logo.png"/>
+        <Logo src="/logo.png"/>
         <List>
             <ListItem onClick={() => handleClickScrollToWho()} >About me</ListItem>
             <ListItem onClick={() => handleClickScrollToWorks()} >Shop</ListItem>
